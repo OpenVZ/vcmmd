OpenVZ/vcmmd

commit 0f7499c8e34232a3ccce6f00a7278b498532e935

config: drop MEM_IDLE_THRESH
It's a low level option, which isn't supposed to be tuned by the admin,
so let's hard-code it.

Also, using 1% threshold looks dubious, because for big containers this
can result in wasting memory. Let's always use 16MB instead.

Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

vcmmd/memcg.py
@@ -186,10 +186,10 @@ class _MemCg(AbstractLoadEntity):
         # We do not need per mem type stats any longer
         total = sum(total.itervalues())
 
-        # If relative share of idle memory is below the threshold, assume the
-        # demand to be increased by pgpgin each update interval. This will give
-        # the memcg a chance to increase its share.
-        demand_high = demand > total * (1 - config.MEM_IDLE_THRESH)
+        # Neglect idle memory if there is too little of it (<16MB). In this
+        # case we assume that the demand will increase by pgpgin each update
+        # interval. This should give the memcg a chance to increase its share.
+        demand_high = demand > 16777216
         demand = demand * ~demand_high
         demand += demand_high * (total + np.arange(1, MAX_AGE + 1) * pgpgin)
 

vcmmd.conf
@@ -26,10 +26,6 @@ MEM_IDLE_SAMPLING_RATIO = 20
 # in seconds
 MEM_IDLE_DELAY = 5
 
-# Do not take into account memory that is not used by a container if its
-# relative portion is less than
-MEM_IDLE_THRESH = 0.1
-
 # Assume a memory page is very unlikely to be used again if it has not been
 # touched for more than MEM_STALE_AGE seconds
 MEM_STALE_AGE = 300

vcmmd/config.py
@@ -32,10 +32,6 @@ _OPTIONS = {
     # in seconds
     'MEM_IDLE_DELAY':                   5,
 
-    # Do not take into account memory that is not used by a container if its
-    # relative portion is less than
-    'MEM_IDLE_THRESH':                  0.1,
-
     # Assume a memory page is very unlikely to be used again if it has not been
     # touched for more than MEM_STALE_AGE seconds
     'MEM_STALE_AGE':                    300,
@@ -64,7 +60,6 @@ def _update_options():
     globals()['MEM_AVAIL'] = MEM_TOTAL - SYSTEM_MEM
     globals()['MEM_IDLE_SAMPLING_RATIO'] = max(MEM_IDLE_SAMPLING_RATIO, 1)
     globals()['MEM_IDLE_DELAY'] = max(MEM_IDLE_DELAY, 1)
-    globals()['MEM_IDLE_THRESH'] = clamp(MEM_IDLE_THRESH, 0., 1.)
     globals()['MEM_STALE_SHIFT'] = clamp(MEM_STALE_AGE / MEM_IDLE_DELAY,
                                          1, MAX_AGE)
     globals()['MEM_IDLE_SHIFT'] = {}

commit c81d334903e3fa586eee88899577b6cb675e9d26

memcg: fix high demand detection
We should neglect idle memory if there is too little of it (<16MB), but
the check is broken - instead of comparing idle memory size with 16MB we
compare demand with 16MB, which results in screwing up the logic. Fix
this.

Fixes: 0f7499c8e342 ("config: drop MEM_IDLE_THRESH")
Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

vcmmd/memcg.py
@@ -189,7 +189,7 @@ class _MemCg(AbstractLoadEntity):
         # Neglect idle memory if there is too little of it (<16MB). In this
         # case we assume that the demand will increase by pgpgin each update
         # interval. This should give the memcg a chance to increase its share.
-        demand_high = demand > 16777216
+        demand_high = demand > total - 16777216
         demand = demand * ~demand_high
         demand += demand_high * (total + np.arange(1, MAX_AGE + 1) * pgpgin)
 