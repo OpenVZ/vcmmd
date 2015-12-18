03195468e44748e66e57c2c8d93e7e6685a3fd31
Vladimir Davydov <vdavydov@virtuozzo.com>
2015-12-18 17:39:21 +0300

    Implement limit configuration methods for CT
    It's trivial for CT - just call corresponding methods of the underlying
    cgroup.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- vcmmd/ve/ct.py
+++ vcmmd/ve/ct.py
@@ -1,8 +1,43 @@
-from vcmmd.ve import VE
-from vcmmd.ve import types as ve_types
+from vcmmd.cgroup import MemoryCgroup
+from vcmmd.ve import VE, Error, types as ve_types
+
+
+class CgroupError(Error):
+    pass
 
 
 class CT(VE):
 
     VE_TYPE = ve_types.CT
     VE_TYPE_NAME = 'CT'
+
+    def __init__(self, name):
+        super(CT, self).__init__(name)
+
+        # Currently, containers' cgroups are located at the first level of the
+        # cgroup hierarchy.
+        self._memcg = MemoryCgroup(name)
+
+    def set_mem_low(self, value):
+        try:
+            self._memcg.write_mem_low(value)
+        except IOError as err:
+            raise CgroupError(err)
+
+    def set_mem_high(self, value):
+        try:
+            self._memcg.write_mem_high(value)
+        except IOError as err:
+            raise CgroupError(err)
+
+    def set_mem_max(self, value):
+        try:
+            self._memcg.write_mem_max(value)
+        except IOError as err:
+            raise CgroupError(err)
+
+    def set_swap_max(self, value):
+        try:
+            self._memcg.write_swap_max(value)
+        except IOError as err:
+            raise CgroupError(err)
