d780aef27864d747f808574867c88bfb95da717a
Vladimir Davydov <vdavydov@virtuozzo.com>
2015-12-24 15:23:24 +0300

    Fix int overflow when writing memory cgroup files
    Memory cgroup controller does not expect input to be greater than
    INT64_MAX, while ve.Config parameters can exceed it.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- vcmmd/cgroup/memory.py
+++ vcmmd/cgroup/memory.py
@@ -1,9 +1,16 @@
 from vcmmd.cgroup.base import Cgroup
+from vcmmd.util import INT64_MAX
 
 
 class MemoryCgroup(Cgroup):
 
     CONTROLLER = 'memory'
+
+    _MEM_VAL_MAX = INT64_MAX
+
+    def _write_file_mem_val(self, filename, value):
+        value = min(value, self._MEM_VAL_MAX)
+        self.write_file_int(filename, value)
 
     def read_mem_current(self):
         return self.read_file_int('memory.usage_in_bytes')
@@ -17,13 +24,13 @@ class MemoryCgroup(Cgroup):
         return self.read_file_int('memory.low')
 
     def write_mem_low(self, val):
-        self.write_file_int('memory.low', val)
+        self._write_file_mem_val('memory.low', val)
 
     def read_mem_high(self):
         return self.read_file_int('memory.high')
 
     def write_mem_high(self, val):
-        self.write_file_int('memory.high', val)
+        self._write_file_mem_val('memory.high', val)
 
     def read_mem_max(self):
         return self.read_file_int('memory.limit_in_bytes')
@@ -33,11 +40,11 @@ class MemoryCgroup(Cgroup):
         memsw = self.read_file_int('memory.memsw.limit_in_bytes')
         swp = max(memsw - mem, 0)
         if val > mem:
-            self.write_file_int('memory.memsw.limit_in_bytes', val + swp)
-            self.write_file_int('memory.limit_in_bytes', val)
+            self._write_file_mem_val('memory.memsw.limit_in_bytes', val + swp)
+            self._write_file_mem_val('memory.limit_in_bytes', val)
         else:
-            self.write_file_int('memory.limit_in_bytes', val)
-            self.write_file_int('memory.memsw.limit_in_bytes', val + swp)
+            self._write_file_mem_val('memory.limit_in_bytes', val)
+            self._write_file_mem_val('memory.memsw.limit_in_bytes', val + swp)
 
     def read_swap_max(self):
         mem = self.read_file_int('memory.limit_in_bytes')
@@ -46,4 +53,4 @@ class MemoryCgroup(Cgroup):
 
     def write_swap_max(self, val):
         mem = self.read_file_int('memory.limit_in_bytes')
-        self.write_file_int('memory.memsw.limit_in_bytes', mem + val)
+        self._write_file_mem_val('memory.memsw.limit_in_bytes', mem + val)
--- vcmmd/util.py
+++ vcmmd/util.py
@@ -1,3 +1,4 @@
+INT64_MAX = int(2**63-1)
 UINT64_MAX = int(2**64-1)
 
 
