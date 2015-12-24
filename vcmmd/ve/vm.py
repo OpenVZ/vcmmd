d63b6321a479bad1690ebabe5e70e09f7c8c925d
Vladimir Davydov <vdavydov@virtuozzo.com>
2015-12-24 18:47:39 +0300

    Add basic support of VMs
    Implement set_mem_high as virDomain.setMemory, set_mem_max as
    virDomain.setMaxMemory. set_mem_low and set_swap_max are no-ops for now.
    Note, handling libvirt connection failures is not implemented.
    Supposedly, it will be done from the dispatcher, i.e. the latter will
    restart vcmmd once it detects libvirtd restart, but it's not clear yet.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- vcmmd/ve/vm.py
+++ vcmmd/ve/vm.py
@@ -1,8 +1,44 @@
-from vcmmd.ve import VE
-from vcmmd.ve import types as ve_types
+from vcmmd.ve import VE, Error, types as ve_types
+
+import libvirt
+
+
+class LibvirtError(Error):
+    pass
 
 
 class VM(VE):
 
     VE_TYPE = ve_types.VM
     VE_TYPE_NAME = 'VM'
+
+    _libvirt_conn = None
+
+    def commit(self):
+        try:
+            if not VM._libvirt_conn:
+                VM._libvirt_conn = libvirt.open('qemu:///system')
+            self._libvirt_domain = VM._libvirt_conn.lookupByName(self.name)
+        except libvirt.libvirtError as err:
+            raise LibvirtError(err)
+
+        super(VM, self).commit()
+
+    def set_mem_high(self, value):
+        value >>= 10  # libvirt wants kB
+        try:
+            self._libvirt_domain.setMemory(value)
+        except libvirt.libvirtError as err:
+            raise LibvirtError(err)
+
+    def set_mem_max(self, value):
+        value >>= 10  # libvirt wants kB
+        try:
+            # libvirt will fail setting MaxMemory on a running VM if it is not
+            # supported by the OS running inside the VM. So, to avoid failing
+            # commit stage false-positively, do not try to adjust MaxMemory if
+            # it is already set to what we want.
+            if self._libvirt_domain.maxMemory() != value:
+                self._libvirt_domain.setMaxMemory(value)
+        except libvirt.libvirtError as err:
+            raise LibvirtError(err)
