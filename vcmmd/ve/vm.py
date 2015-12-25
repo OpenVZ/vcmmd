4b1d4d90aeaf63cc151b69c15f5705a6c1e49ade
Vladimir Davydov <vdavydov@virtuozzo.com>
2015-12-25 19:18:58 +0300

    Fetch memory statistics for virtual machines
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- vcmmd/ve/vm.py
+++ vcmmd/ve/vm.py
@@ -1,4 +1,4 @@
-from vcmmd.ve import VE, Error, types as ve_types
+from vcmmd.ve import VE, Error, types as ve_types, MemStats
 
 import libvirt
 
@@ -12,7 +12,13 @@ class VM(VE):
     VE_TYPE = ve_types.VM
     VE_TYPE_NAME = 'VM'
 
+    _MEMSTAT_PERIOD = 5  # seconds
+
     _libvirt_conn = None
+
+    def __init__(self, name):
+        super(VM, self).__init__(name)
+        self._mem_stats_enabled = False
 
     def commit(self):
         try:
@@ -23,6 +29,30 @@ class VM(VE):
             raise LibvirtError(err)
 
         super(VM, self).commit()
+
+    def _fetch_mem_stats(self):
+        try:
+            if not self._mem_stats_enabled:
+                # libvirt must be explicitly told to collect memory statistics
+                self._libvirt_domain.setMemoryStatsPeriod(self._MEMSTAT_PERIOD)
+                self._mem_stats_enabled = True
+            stat = self._libvirt_domain.memoryStats()
+        except libvirt.libvirtError as err:
+            raise LibvirtError(err)
+
+        try:
+            used = max(stat['available'] - stat['unused'], 0)
+        except KeyError:
+            # Guest driver is not installed? Use qemu process rss then.
+            used = stat['rss']
+
+        # libvirt reports memory values in kB, so we need to convert them to
+        # bytes
+        return MemStats(actual=stat['actual'] << 10,
+                        rss=stat['rss'] << 10,
+                        used=used << 10,
+                        minflt=stat.get('minor_fault', 0),
+                        majflt=stat.get('major_fault', 0))
 
     def set_mem_high(self, value):
         value >>= 10  # libvirt wants kB
