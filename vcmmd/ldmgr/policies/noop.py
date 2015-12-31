67ea6ab01bc3d484d7bd5c0043a40f438a222615
Vladimir Davydov <vdavydov@virtuozzo.com>
2015-12-31 13:10:22 +0300

    Improve comment to no-op load manager policy
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- vcmmd/ldmgr/policies/noop.py
+++ vcmmd/ldmgr/policies/noop.py
@@ -4,8 +4,10 @@ from vcmmd.ldmgr import PolicyWithGuarantees
 class NoOpPolicy(PolicyWithGuarantees):
     '''No Operation load manager policy.
 
-    Assume optimal memory consumption range for a VE is (guarantee, limit).
-    That's it, simple as that.
+    Set best-effort memory protection and throttle limit to configured
+    guarantee and limit, respectively, and let the host kernel do the rest.
+    This will only work satisfactory if the host kernel can reclaim memory from
+    VEs effectively and is smart enough to detect a VE's working set by itself.
     '''
 
     def balance(self, all_ves, timeout):
