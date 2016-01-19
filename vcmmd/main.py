5efa0b7fd88e2a762fe94a971bed2b370432656e
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-01-19 17:51:36 +0300

    Cleanup daemon main function code
    There are too many global variables involved, which looks ugly. Let's
    move the code to a class to clean things up.
    
    No functional changes are intended.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- vcmmd/main.py
+++ vcmmd/main.py
@@ -15,109 +15,124 @@ from vcmmd.ldmgr import LoadManager
 from vcmmd.rpc.dbus import RPCServer
 from vcmmd.util.logging import LoggerWriter
 
-PID_FILE = '/var/run/vcmmd.pid'
-LOG_FILE = '/var/log/vcmmd.log'
 
-INIT_SCRIPTS_DIR = '/etc/vz/vcmmd/init.d'
+class _App(object):
 
-_should_stop = False
+    PID_FILE = '/var/run/vcmmd.pid'
+    LOG_FILE = '/var/log/vcmmd.log'
 
+    INIT_SCRIPTS_DIR = '/etc/vz/vcmmd/init.d'
 
-def _sighandler(signum, frame):
-    global _should_stop
-    _should_stop = True
+    def __init__(self):
+        self.parse_args()
+        self.init_logging()
 
+        if self.opts.interactive:
+            self.run_interactive()
+        else:
+            self.run_daemon()
 
-def _run_one_init_script(script):
-    logger = logging.getLogger('vcmmd')
+    def parse_args(self):
+        parser = optparse.OptionParser("Usage: %prog [-i] [-d]")
+        parser.add_option("-i", action="store_true", dest="interactive",
+                          help="run interactive (not a daemon)")
+        parser.add_option("-d", action="store_true", dest="debug",
+                          help="increase verbosity to debug level")
 
-    logger.info("Running init script '%s'", script)
+        (opts, args) = parser.parse_args()
+        if args:
+            parser.error("incorrect number of arguments")
 
-    try:
-        with open(os.devnull, 'r') as devnull:
-            p = subprocess.Popen(os.path.join(INIT_SCRIPTS_DIR, script),
-                                 stdout=devnull, stderr=subprocess.PIPE)
-        stdout, stderr = p.communicate()
-    except OSError as err:
-        logger.error("Error running init script '%s': %s", script, err)
-        return
+        self.opts = opts
 
-    if p.returncode != 0:
-        logger.error("Script '%s' returned %s, stderr output:\n%s",
-                     script, p.returncode, stderr)
+    def init_logging(self):
+        logger = logging.getLogger('vcmmd')
+        logger.setLevel(logging.DEBUG if self.opts.debug else logging.INFO)
 
+        fmt = logging.Formatter(
+            "%(asctime)s %(levelname)s %(name)s: %(message)s",
+            "%Y-%m-%d %H:%M:%S")
 
-def _run_init_scripts():
-    logger = logging.getLogger('vcmmd')
+        fh = logging.StreamHandler() if self.opts.interactive else \
+            logging.FileHandler(self.LOG_FILE)
+        fh.setFormatter(fmt)
 
-    if not os.path.isdir(INIT_SCRIPTS_DIR):
-        return
+        logger.addHandler(fh)
 
-    try:
-        scripts = os.listdir(INIT_SCRIPTS_DIR)
-    except OSError as err:
-        logger.error('Failed to read init scripts dir: %s', err)
-        return
+        self.logger = logger
+        self.logger_stream = fh.stream  # for DaemonContext:files_preserve
 
-    for script in sorted(scripts):
-        if not script.startswith('.'):
-            _run_one_init_script(script)
+    def run_one_init_script(self, script):
+        self.logger.info("Running init script '%s'", script)
 
+        try:
+            with open(os.devnull, 'r') as devnull:
+                p = subprocess.Popen(
+                    os.path.join(self.INIT_SCRIPTS_DIR, script),
+                    stdout=devnull, stderr=subprocess.PIPE)
+            stdout, stderr = p.communicate()
+        except OSError as err:
+            self.logger.error("Error running init script '%s': %s",
+                              script, err)
+            return
 
-def _run():
-    # DaemonContext closes stdout and stderr, redirect them to the logger.
-    logger = logging.getLogger('vcmmd')
-    sys.stdout = LoggerWriter(logger, logging.INFO)
-    sys.stderr = LoggerWriter(logger, logging.CRITICAL)
+        if p.returncode != 0:
+            self.logger.error("Script '%s' returned %s, stderr output:\n%s",
+                              script, p.returncode, stderr)
 
-    logger.info('Started')
+    def run_init_scripts(self):
+        if not os.path.isdir(self.INIT_SCRIPTS_DIR):
+            return
 
-    ldmgr = LoadManager()
-    rpcsrv = RPCServer(ldmgr)
+        try:
+            scripts = os.listdir(self.INIT_SCRIPTS_DIR)
+        except OSError as err:
+            self.logger.error('Failed to read init scripts dir: %s', err)
+            return
 
-    _run_init_scripts()
+        for script in sorted(scripts):
+            if not script.startswith('.'):
+                self.run_one_init_script(script)
 
-    # threading.Event would fit better here, but it ignores signals.
-    while not _should_stop:
-        time.sleep(1)
+    def run(self):
+        # Redirect stdout and stderr to logger
+        sys.stdout = LoggerWriter(self.logger, logging.INFO)
+        sys.stderr = LoggerWriter(self.logger, logging.CRITICAL)
 
-    rpcsrv.shutdown()
-    ldmgr.shutdown()
+        self.logger.info('Started')
 
-    logger.info('Stopped')
+        ldmgr = LoadManager()
+        rpcsrv = RPCServer(ldmgr)
+
+        self.run_init_scripts()
+
+        # threading.Event would fit better here, but it ignores signals.
+        self.should_stop = False
+        while not self.should_stop:
+            time.sleep(1)
+
+        rpcsrv.shutdown()
+        ldmgr.shutdown()
+
+        self.logger.info('Stopped')
+
+    def sighandler(self, signum, frame):
+        self.should_stop = True
+
+    def run_interactive(self):
+        signal.signal(signal.SIGINT, self.sighandler)
+        self.run()
+
+    def run_daemon(self):
+        with daemon.DaemonContext(
+                pidfile=daemon.pidfile.TimeoutPIDLockFile(self.PID_FILE, -1),
+                files_preserve=[self.logger_stream],
+                signal_map={signal.SIGTERM: self.sighandler}):
+            self.run()
 
 
 def main():
-    parser = optparse.OptionParser("Usage: %prog [-i] [-d]")
-    parser.add_option("-i", action="store_true", dest="interactive",
-                      help="run interactive (not a daemon)")
-    parser.add_option("-d", action="store_true", dest="debug",
-                      help="increase verbosity to debug level")
-
-    (opts, args) = parser.parse_args()
-    if args:
-        parser.error("incorrect number of arguments")
-
-    logger = logging.getLogger('vcmmd')
-    logger.setLevel(logging.DEBUG if opts.debug else logging.INFO)
-    fmt = logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s",
-                            "%Y-%m-%d %H:%M:%S")
-    fh = logging.StreamHandler() if opts.interactive else \
-        logging.FileHandler(LOG_FILE)
-    fh.setFormatter(fmt)
-    logger.addHandler(fh)
-
-    if opts.interactive:
-        signal.signal(signal.SIGINT, _sighandler)
-        _run()
-    else:
-        context = daemon.DaemonContext(
-            pidfile=daemon.pidfile.TimeoutPIDLockFile(PID_FILE, -1),
-            files_preserve=[fh.stream],
-            signal_map={signal.SIGTERM: _sighandler}
-        )
-        with context:
-            _run()
+    _App()
 
 if __name__ == "__main__":
     main()
