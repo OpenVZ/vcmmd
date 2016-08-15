a9fca0be5ceb92684ae5ea54a55316f63cc68eff
Derbyshev Dmitry <dderbyshev@virtuozzo.com>
2016-08-15 19:25:46 +0300

    numa: Cpu and CpuStat classes added
    /proc/stat is being parsed for cpu-wise resources.
    
    Signed-off-by: Derbyshev Dmitry <dderbyshev@virtuozzo.com>
    Signed-off-by: Igor Redko <redkoi@virtuozzo.com>

--- vcmmd/numa.py
+++ vcmmd/numa.py
@@ -17,7 +17,64 @@
 #
 # Our contact details: Parallels IP Holdings GmbH, Vordergasse 59, 8200
 # Schaffhausen, Switzerland.
+import re, os, psutil
+
 from vcmmd.util.singleton import Singleton
+from vcmmd.util.stats import Stats
 
 class Numa(object):
     __metaclass__ = Singleton
+
+    def __init__(self):
+        self.cpus = {i: CPU(i) for i in self.get_logical_cpus_ids()}
+
+    def get_num_logical_cpus(self):
+        return psutil.cpu_count(logical = True)
+
+    def get_logical_cpus_ids(self):
+        #FIXME: may be non-sequential, but numad had not cared about this.
+        return range(self.get_num_logical_cpus())
+
+    def read_cpu_stats(self):
+        stats = open("/proc/stat", "r").readlines()
+        for cpu in self.cpus.values():
+            cpu.update_stats(stats)
+
+    def update_stats(self):
+        self.read_cpu_stats()
+
+    def __str__(self):
+        res = "CPUs:\n"
+        for cpu in self.cpus.values():
+            res += "\t%s\n" % cpu
+        return res
+
+class CPUStats(Stats):
+
+    ABSOLUTE_STATS = []
+
+    CUMULATIVE_STATS = [
+        'idle'                # percentage of time spent in the idle task
+    ]
+
+    ALL_STATS = ABSOLUTE_STATS + CUMULATIVE_STATS
+
+    def __str__(self):
+        return str({x : getattr(self, x, None) for x in self.ALL_STATS})
+
+
+class CPU(object):
+    def __init__(self, id):
+        self.id = id
+        self.stats = CPUStats()
+
+    def update_stats(self, stat = None):
+        if not stat:
+            stat = open("/proc/stat", "r").readlines()
+        line = [x for x in stat if ("cpu%s" % self.id) in x][0]
+        idle = int(line.split()[4])
+        self.stats._update(**{"idle" : idle})
+
+    def __str__(self):
+        l = ["stats"]
+        return ("CPU %s:%s" % (self.id, {x : str(self.__dict__[x]) for x in l}))
