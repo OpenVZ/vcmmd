d14cf30d9fbb583aa4d95c7f2b9bcc0f095eb5d7
Konstantin Neumoin <kneumoin@virtuozzo.com>
2016-09-11 16:04:55 +0300

    add vcmmd:env
    PSBM-51051
    THis module contain abstract Env class, it will in future basic for
    VE/Host classes.
    Need for unification logging and update methods.
    
    Signed-off-by: root <root@p3.perf.qa.sw.ru>

---
+++ vcmmd/env.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2016 Parallels IP Holdings GmbH
+#
+# This file is part of OpenVZ. OpenVZ is free software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# Our contact details: Parallels IP Holdings GmbH, Vordergasse 59, 8200
+# Schaffhausen, Switzerland.
+
+import logging
+from abc import ABCMeta, abstractmethod
+
+
+class Env(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def update_stats(self):
+        pass
+
+    @abstractmethod
+    def get_cpu_stats(self):
+        pass
+
+    @abstractmethod
+    def get_numa_stats(self):
+        pass
+
+    def __init__(self, name):
+        self.__logger = logging.getLogger(name)
+
+    def __log(self, lvl, msg, *args, **kwargs):
+        self.__logger.log(lvl, str(self) + ': ' + msg, *args, **kwargs)
+
+    def log_err(self, *args, **kwargs):
+        self.__log(logging.ERROR, *args, **kwargs)
+
+    def log_info(self, *args, **kwargs):
+        self.__log(logging.INFO, *args, **kwargs)
+
+    def log_debug(self, *args, **kwargs):
+        # Debugging is unlikely to be enabled.
+        # Avoid evaluating args if it is not.
+        if self.__logger.isEnabledFor(logging.DEBUG):
+            self.__log(logging.DEBUG, *args, **kwargs)
