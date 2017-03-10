af5fa55b75ae0fb95dcff885f7537bf7695cc808
Andrey Bondrov <abondrov@virtuozzo.com>
2017-03-10 21:45:11 +1000

    Update copyrights

--- vcmmd/rpc/dbus/server.py
+++ vcmmd/rpc/dbus/server.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Parallels IP Holdings GmbH
+# Copyright (c) 2016-2017, Parallels International GmbH
 #
 # This file is part of OpenVZ. OpenVZ is free software; you can redistribute
 # it and/or modify it under the terms of the GNU General Public License as
@@ -15,7 +15,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 #
-# Our contact details: Parallels IP Holdings GmbH, Vordergasse 59, 8200
+# Our contact details: Parallels International GmbH, Vordergasse 59, 8200
 # Schaffhausen, Switzerland.
 
 from __future__ import absolute_import
