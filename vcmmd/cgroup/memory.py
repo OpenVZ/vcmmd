a6f44991cb713332ceefe2d795236fafc8adfd28
Vyacheslav Trushkin <vyacheslav.trushkin@virtuozzo.com>
2021-08-23 18:47:37 +0300

    Remove unused idlememscanner

--- vcmmd/cgroup/memory.py
+++ vcmmd/cgroup/memory.py
@@ -20,94 +20,10 @@
 # Schaffhausen, Switzerland.
 
 import re
-import time
-import threading
 
-from vcmmd.cgroup import idlememscan
 from vcmmd.cgroup.base import Cgroup
 from vcmmd.util.limits import INT64_MAX
-from vcmmd.util.singleton import Singleton
 from vcmmd.util.limits import PAGE_SIZE
-
-
-class _IdleMemScanner(metaclass=Singleton):
-
-    class _StopScan(Exception):
-        pass
-
-    def __init__(self):
-        self.__result = {}
-        self.__sampling = 1
-        self.__period = 0
-        self.__scan_in_progress = False
-        self.__lock = threading.Lock()
-
-    def __do_scan_iter(self):
-        if self.__restart_scan:
-            with self.__lock:
-                period = self.__period
-                if period <= 0:
-                    self.__scan_in_progress = False
-                    raise self._StopScan
-            idlememscan.set_sampling(self.__sampling)
-            self.__scan_took = 0.0
-            self.__time_left = float(period)
-            self.__restart_scan = False
-
-        start = time.time()
-        iters_done, iters_left = idlememscan.iter()
-        spent = time.time() - start
-
-        self.__scan_took += spent
-        self.__time_left -= spent
-
-        scan_will_take = iters_left * self.__scan_took / iters_done
-        if scan_will_take < self.__time_left:
-            timeout = ((self.__time_left - scan_will_take) / iters_left
-                       if iters_left > 0 else self.__time_left)
-            time.sleep(timeout)
-            self.__time_left -= timeout
-
-        if iters_left == 0:
-            self.__result = idlememscan.result()
-            self.__restart_scan = True
-
-    def __scan_fn(self):
-        self.__restart_scan = True
-        try:
-            while True:
-                self.__do_scan_iter()
-        except self._StopScan:
-            pass
-
-    def set_sampling(self, sampling):
-        if not isinstance(sampling, float):
-            raise TypeError("'sampling' must be a float")
-        if not 0.0 < sampling <= 1.0:
-            raise ValueError("'sampling' must be in range (0.0, 1.0]")
-
-        self.__sampling = sampling
-
-    def set_period(self, period):
-        if not isinstance(period, int):
-            raise TypeError("'period' must be an integer")
-        if period < 0:
-            raise ValueError("'period' must be >= 0")
-
-        start_scan = False
-        with self.__lock:
-            self.__period = period
-            if period > 0 and not self.__scan_in_progress:
-                self.__scan_in_progress = True
-                start_scan = True
-        if start_scan:
-            t = threading.Thread(target=self.__scan_fn)
-            t.daemon = True
-            t.start()
-
-    @property
-    def result(self):
-        return self.__result
 
 
 class MemoryCgroup(Cgroup):
@@ -187,63 +103,6 @@ class MemoryCgroup(Cgroup):
 
     def read_cache_limit_in_bytes(self):
         return self._read_file_int('cache.limit_in_bytes')
-
-    @staticmethod
-    def set_idle_mem_period(period):
-        '''Set idle memory scan period, in seconds.
-
-        If enabled, idle memory scanner will periodically scan physical memory
-        range and count pages that have not been touched since the previous
-        scan. The result can be obtained with 'get_idle_factor'.
-
-        If 'period' is 0, the scanner will be stopped.
-
-        Note, the change will only take place after the current period
-        completes.
-        '''
-        _IdleMemScanner().set_period(period)
-
-    @staticmethod
-    def set_idle_mem_sampling(sampling):
-        '''Set idle memory sampling.
-
-        Set the portion of memory to check while performing idle scan.
-
-        Note, the change will only take place after the current period
-        completes.
-        '''
-        _IdleMemScanner().set_sampling(sampling)
-
-    def _get_idle_factor(self, mem_types):
-        try:
-            stat = _IdleMemScanner().result[self.path]
-        except KeyError:
-            # No stats yet? Assume all memory is active.
-            return 0.
-
-        total = sum(stat[i * 2] for i in mem_types)
-        idle = sum(stat[i * 2 + 1] for i in mem_types)
-
-        # avoid div/0
-        return float(idle) / (total + 1)
-
-    def get_idle_factor(self):
-        '''Return the percentage of ageable memory that was not touched during
-        the last scan.
-        '''
-        return self._get_idle_factor([0, 1])
-
-    def get_idle_factor_anon(self):
-        '''Return the percentage of anonymous memory that was not touched
-        during the last scan.
-        '''
-        return self._get_idle_factor([0])
-
-    def get_idle_factor_file(self):
-        '''Return the percentage of memory used for storing file caches that
-        was not touched during the last scan.
-        '''
-        return self._get_idle_factor([1])
 
     def get_numa_stats(self):
         stats = self._read_file_str("numa_stat")
