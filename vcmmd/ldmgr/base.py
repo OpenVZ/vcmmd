OpenVZ/vcmmd

commit 3d69ee7f4223d7e10b3ece3e77d6b11c343f3dd5

Respect cache limit value when setting VM config #VSTOR-82456
We have a separate config parameter for page cache limit, but it's not
being used when setting VM config.  Let's fix that.

If parameter isn't passed at all, we're using default value (currently
it's 512M for VMs).

Signed-off-by: Andrey Drobyshev <andrey.drobyshev@virtuozzo.com>

vcmmd/ldmgr/base.py
@@ -44,6 +44,7 @@ from vcmmd.config import VCMMDConfig
 from vcmmd.ve import VE
 from vcmmd.ve.base import Error
 from vcmmd.ve.ct import lookup_cgroup
+from vcmmd.ve.vm import VM_DEFAULT_CACHE_LIMIT_MB
 from vcmmd.host import Host
 from vcmmd.cgroup.memory import MemoryCgroup
 from vcmmd.cgroup.cpu import CpuCgroup
@@ -189,6 +190,11 @@ class LoadManager:
         with self._registered_ves_lock:
             if ve_name in self._registered_ves:
                 raise VCMMDError(VCMMD_ERROR_VE_NAME_ALREADY_IN_USE)
+
+            if ve_type != VE_TYPE_SERVICE:
+                ve_config.complete(cache=int(
+                    VCMMDConfig().get("LoadManager.Controllers.VMCacheLimitTotal",
+                                      VM_DEFAULT_CACHE_LIMIT_MB * 1024 * 1024)))
 
             ve_config.complete(DefaultVEConfig)
             if ve_type != VE_TYPE_SERVICE and \
@@ -465,6 +471,9 @@ class LoadManager:
             guarantee_auto = guarantee_dom.attrib.get('vz-auto', False)
             if guarantee_auto != 'yes':
                 guarantee_pct = int(guarantee_dom.text)
+        cache_limit_dom = dom_xml.find('./memtune/page_cache_limit')
+        if cache_limit_dom:
+            ve_config['cache'] = int(cache_limit_dom.text)
         ve_config['guarantee'] = ve_config['limit'] * guarantee_pct // 100
         ve_config['guarantee_type'] = guarantee_auto != 'yes'
         self.register_ve(uuid, ve_type, VEConfig(**ve_config))

vcmmd/ve/vm.py
@@ -241,11 +241,7 @@ class VMImpl(VEImpl):
             raise Error("Cgroup write failed: {}".format(err))
 
         # Set memory.cache.limit_in_bytes to limit cache generated by backup
-        cache_limit = VCMMDConfig().get(
-            "LoadManager.Controllers.VMCacheLimitTotal",
-            VM_DEFAULT_CACHE_LIMIT_MB * 1024 * 1024,
-        )
-        self._memcg.write_cache_limit_in_bytes(cache_limit)
+        self._memcg.write_cache_limit_in_bytes(config.cache)
 
         # Update memory limit
         value = config.limit

commit 04bc5d10fc68116af0d775abd8121371c580fb19

FIXUP: ldmgr: proper comletion of VE config #PSBM-156291
fixup! Respect cache limit value when setting VM config #VSTOR-82456
Fixes: 3d69ee7f4223d7e10b3ece3e77d6b11c343f3dd5

Cache value should be passed to config allocator first, and then
config is passed to complete() method.

Signed-off-by: Nikolai Barybin <nikolai.barybin@virtuozzo.com>

vcmmd/ldmgr/base.py
@@ -192,9 +192,9 @@ class LoadManager:
                 raise VCMMDError(VCMMD_ERROR_VE_NAME_ALREADY_IN_USE)
 
             if ve_type != VE_TYPE_SERVICE:
-                ve_config.complete(cache=int(
+                ve_config.complete(VEConfig(cache=int(
                     VCMMDConfig().get("LoadManager.Controllers.VMCacheLimitTotal",
-                                      VM_DEFAULT_CACHE_LIMIT_MB * 1024 * 1024)))
+                                      VM_DEFAULT_CACHE_LIMIT_MB * 1024 * 1024))))
 
             ve_config.complete(DefaultVEConfig)
             if ve_type != VE_TYPE_SERVICE and \